06637279eecef55b6ce0f71d49a65b9d073e6e15
JulietaLezcano <60350457+JulietaLezcano@users.noreply.github.com>
2020-01-30 09:12:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+ namespace Productoria
+ {
+     class Program
+     {
+            static void Main (string[] args)
+            {
+                Console.Write("Ingrese el primer numero: ");
+                int n1= Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingrese el segundo numero: ");
+                int n2= Convert.ToInt32(Console.ReadLine());
+                int Producto = 0;
+                for (int i=0;  i<n2; i++)
+                {
+                    
+                    Producto += n2;
+                }
+                Console.WriteLine(Producto);
+            }
+     }
+ }
+
